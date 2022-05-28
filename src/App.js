6a7770b5e2e5c2423626a20734163f861f9b7c1c
Alejandro Quiñonez <alquinonez.dwfc5@ucamp.utel.edu.mx>
2022-05-27 18:56:13 -0600

    agregado ejercicio 2 de Routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
-import CardQuote from './Components/CardQuote/CardQuote';
 
 function App() {
 
-  const [quote, setQuote] = useState({})
-  
-  const getQuotes = async () => {
-    const url = 'https://www.breakingbadapi.com/api/quote/random'
-    const resp = await axios.get(url)
-    setQuote(resp.data[0])
-
-    console.log(resp.data[0])
-  }
-
-  useEffect(()=>{
-    getQuotes() //Use effect en este caso para evitar el ciclado de la app
-  }, [])
-
-    
+ 
     return (
       <div className='App'>
         <header className='App-header'>
-          <CardQuote info={ quote } />
-          <button onClick={ ()=>getQuotes()} style={{
-            padding: '15px 35px 15px 35px',
-            cursor: 'pointer',
-            fontSize: '22px'
-
-          }}>
-            Click
-          </button>
         </header>
       </div>
     )
